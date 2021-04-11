0a4c6f80ade9329718f2fd041c7dfa6dfe50f949
Sam Bannister <sambannister@gmail.com>
2021-04-11 14:08:18 +0100

    Convert from CRLF to LF

--- bin/upgrade_containers.sh
+++ bin/upgrade_containers.sh
@@ -26,7 +26,7 @@ fi
 
 sudo -E docker-compose \
     -f /home/pi/screenly/docker-compose.yml \
-    -f /home/pi/screenly/docker-compose.override.yml \
+    -f /home/pi/screenly/docker-compose.override.yml \s
     pull
 
 sudo -E docker-compose \
---
+++ docs/install.md
@@ -0,0 +1,1 @@
+bash <(curl -sL https://raw.githubusercontent.com/kenban/kenban_os/master/bin/install.sh)
