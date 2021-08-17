d9613bca5d94773592408152fbbb002436bc626b
Sam Bannister <sambannister@gmail.com>
2021-08-17 17:58:16 +0100

    moved websocket to separate module

--- kenban/sync.py
+++ kenban/sync.py
@@ -1,4 +1,3 @@
-import asyncio
 import json
 import logging
 import os
@@ -23,37 +22,6 @@ celery = Celery(
     broker=CELERY_BROKER_URL,
     result_expires=CELERY_TASK_RESULT_EXPIRES
 )
-
-
-async def subscribe_to_updates():
-    """ Open a websocket connection with the server. When the string 'updated' is sent, trigger a sync of assets """
-    import websockets
-    url = k_settings["websocket_updates_address"] + k_settings["device_uuid"]
-    while True:
-        # outer loop restarted every time the connection fails
-        try:
-            async with websockets.connect(url) as ws:
-                print("Websocket connected")
-                while True:
-                    try:
-                        msg = await asyncio.wait_for(ws.recv(), timeout=10)
-                        if "updated" in msg:
-                            full_sync()
-                    except (asyncio.TimeoutError, websockets.ConnectionClosed):
-                        try:
-                            pong = await ws.ping()
-                            await asyncio.wait_for(pong, timeout=10)
-                            logging.debug('Ping OK, keeping connection alive...')
-                            continue
-                        except:
-                            await asyncio.sleep(9)
-                            break  # inner loop
-        except socket.gaierror:
-            logging.error("Websocket error")
-            continue
-        except ConnectionRefusedError:
-            logging.error("Websocket connection refused")
-            continue
 
 
 def full_sync():
@@ -210,6 +178,3 @@ def update_schedule(force=False):
 def setup_periodic_kenban_tasks(sender, **kwargs):
     sender.add_periodic_task(10, update_schedule.s(), name='schedule_update')
 
-
-if __name__ == "__main__":
-    asyncio.run(subscribe_to_updates())
---
+++ kenban/websocket.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+
+import websockets
+
+from kenban.settings_kenban import settings as k_settings
+from kenban.sync import full_sync
+
+
+async def subscribe_to_updates():
+    """ Open a websocket connection with the server. When the string 'updated' is sent, trigger a sync of assets """
+    url = k_settings["websocket_updates_address"] + k_settings["device_uuid"]
+    while True:
+        # outer loop restarted every time the connection fails
+        try:
+            async with websockets.connect(url) as ws:
+                print("Websocket connected")
+                while True:
+                    try:
+                        msg = await asyncio.wait_for(ws.recv(), timeout=10)
+                        if "updated" in msg:
+                            full_sync()
+                    except (asyncio.TimeoutError, websockets.ConnectionClosed):
+                        try:
+                            pong = await ws.ping()
+                            await asyncio.wait_for(pong, timeout=10)
+                            logging.debug('Ping OK, keeping connection alive...')
+                            continue
+                        except:
+                            await asyncio.sleep(9)
+                            break  # inner loop
+        except socket.gaierror:
+            logging.error("Websocket error")
+            continue
+        except ConnectionRefusedError:
+            logging.error("Websocket connection refused")
+            continue
+
+
+if __name__ == "__main__":
+    asyncio.run(subscribe_to_updates())
