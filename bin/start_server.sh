4c8a950f8d85f300fc34701eaf073b8f845da461
Sam Bannister <sambannister@gmail.com>
2021-10-23 15:52:18 +0100

    remove migration

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -3,24 +3,16 @@
 mkdir -p \
     /data/.config \
     /data/.kenban \
-    /data/screenly_assets
+    /data/kenban_assets/kenban_templates \
+    /data/kenban_assets/kenban_images
 
 cp -n /usr/src/app/ansible/roles/kenban/files/kenban.conf /data/.kenban/kenban.conf
 cp -n /usr/src/app/ansible/roles/kenban/files/default_assets.yml /data/.kenban/default_assets.yml
 cp -n /usr/src/app/ansible/roles/kenban/files/kenban.db /data/.kenban/kenban.db
 
 if [ -n "${OVERWRITE_CONFIG}" ]; then
-    echo "Requested to overwrite Screenly config file."
+    echo "Requested to overwrite config file."
     cp /usr/src/app/ansible/roles/kenban/files/kenban.conf "/data/.kenban/kenban.conf"
 fi
-
-# Set management page's user and password from environment variables,
-# but only if both of them are provided. Can have empty values provided.
-if [ -n "${MANAGEMENT_USER+x}" ] && [ -n "${MANAGEMENT_PASSWORD+x}" ]; then
-    sed -i -e "s/^user=.*/user=${MANAGEMENT_USER}/" -e "s/^password=.*/password=${MANAGEMENT_PASSWORD}/" /data/.screenly/kenban.conf
-fi
-
-echo "Running migration..."
-python3 ./bin/migrate.py
 
 python3 server.py
