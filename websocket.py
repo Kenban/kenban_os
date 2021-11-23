a93a08b06575e61bbba9f6d4238cfbabbf0d8358
Sam Bannister <sambannister@gmail.com>
2021-11-23 01:44:35 +0000

    Added authentication to websocket

--- websocket.py
+++ websocket.py
@@ -7,6 +7,7 @@ import socket
 from websockets.exceptions import WebSocketException
 
 import sync
+from authentication import get_access_token
 from lib.db_helper import create_or_update_schedule_slot, create_or_update_event
 from lib.models import Session
 from settings import settings
@@ -17,13 +18,25 @@ async def subscribe_to_updates():
     while True:
         # Don't try and open a websocket if we don't have a device uuid yet
         if settings["device_uuid"] in [None, "None", ""]:
+            logging.warning("Unable to start websocket: No device UUID")
             await asyncio.sleep(5)
             continue
         url = settings["websocket_updates_address"] + settings["device_uuid"]
         # outer loop restarted every time the connection fails
         try:
             async with websockets.connect(url) as ws:
-                print("Websocket connected")
+                # Send the access token to authenticate
+                logging.info("Attempting to connect to websocket")
+                try:
+                    access_token = get_access_token()
+                    await ws.send(access_token)
+                    auth_response = await asyncio.wait_for(ws.recv(), timeout=10)
+                    logging.info(f"Authentication response: {auth_response}")
+                    if auth_response != "success":
+                        logging.error("Failed to authenticate websocket")
+                except (asyncio.TimeoutError, websockets.ConnectionClosed):
+                    logging.error("Error authenticating websocket")
+                logging.info("Websocket connected")
                 while True:
                     try:
                         msg = await asyncio.wait_for(ws.recv(), timeout=10)
@@ -78,5 +91,5 @@ if __name__ == "__main__":
     settings.load()
     logging.getLogger().setLevel(logging.DEBUG if settings['debug_logging'] else logging.INFO)
 
-    sync.full_sync()
+    #sync.full_sync()
     asyncio.run(subscribe_to_updates())
