06bcab507e15125a3e67092d8e265866a3f1c834
Sam Bannister <sambannister@gmail.com>
2022-10-28 21:27:57 +0100

    Simplified pi-gen files locations

--- pi-gen-files/01-run.sh
+++ pi-gen-files/01-run.sh
@@ -5,18 +5,14 @@ mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_templates"
 mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban"
 
 cp -r files/kenban_os/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban"
+cp -r files/kenban_os/images/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_images"
+cp -r files/kenban_os/templates/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_templates"
 
-install -m 664 files/.profile "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile"
-install -m 664 files/.xinitrc "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
-
-
-cp -r files/images/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_images"
-cp -r files/templates/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_templates"
-
-install -m 644 files/websocket-sync.service "${ROOTFS_DIR}/etc/systemd/system/websocket-sync.service"
-install -m 644 files/kenban-wifi-manager.service "${ROOTFS_DIR}/etc/systemd/system/kenban-wifi-manager.service"
-
-install -m 644 files/kenbanxorg.conf "${ROOTFS_DIR}/etc/X11/xorg.conf.d/kenban.conf"
+install -m 664 files/kenban_os/pi-gen-files/.profile "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile"
+install -m 664 files/kenban_os/pi-gen-files/.xinitrc "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
+install -m 644 files/kenban_os/pi-gen-files/websocket-sync.service "${ROOTFS_DIR}/etc/systemd/system/websocket-sync.service"
+install -m 644 files/kenban_os/pi-gen-files/kenban-wifi-manager.service "${ROOTFS_DIR}/etc/systemd/system/kenban-wifi-manager.service"
+install -m 644 files/kenban_os/pi-gen-files/kenbanxorg.conf "${ROOTFS_DIR}/etc/X11/xorg.conf.d/kenban.conf"
 
 on_chroot << EOF
   
@@ -30,7 +26,5 @@ on_chroot << EOF
   chmod 755 /home/${FIRST_USER_NAME}/kenban/network/wifi-connect
   chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/kenban
   chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/data
-
-  (crontab -l 2>/dev/null; echo "$(( RANDOM % 60)) $(( RANDOM % 8)) $(( RANDOM % 30 + 1)) * * ansible-pull -U https://github.com/kenban/kenban_os/ -C master") | crontab -
 
 EOF
