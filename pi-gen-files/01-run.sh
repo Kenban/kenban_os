8e12aef5582fdf3401050a78954820400426c7d4
Sam Bannister <sambannister@gmail.com>
2023-03-13 23:28:45 +0000

    Do file operations on chroot

--- pi-gen-files/01-run.sh
+++ pi-gen-files/01-run.sh
@@ -1,27 +1,32 @@
 #!/bin/bash -e
 
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_images"
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_templates"
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/user_templates"
-mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban"
-
-git clone https://github.com/Kenban/kenban_os.git "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban"
-cp -r "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/images/*" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_images"
-cp -r "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/templates/*" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/default_templates"
-cp "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/templates/macros.html" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/data/user_templates/"
-
-install -m 664 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/.profile" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile"
-install -m 664 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/.xinitrc" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
-install -m 644 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/websocket-sync.service" "${ROOTFS_DIR}/etc/systemd/system/websocket-sync.service"
-install -m 644 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/websocket-local.service" "${ROOTFS_DIR}/etc/systemd/system/websocket-local.service"
-install -m 644 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/kenban-wifi-manager.service" "${ROOTFS_DIR}/etc/systemd/system/kenban-wifi-manager.service"
-install -m 644 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/kenban/pi-gen-files/kenbanxorg.conf" "${ROOTFS_DIR}/etc/X11/xorg.conf.d/kenban.conf"
-
 on_chroot << EOF
   
+  mkdir -p "/home/${FIRST_USER_NAME}/data/default_images"
+  mkdir -p "/home/${FIRST_USER_NAME}/data/default_templates"
+  mkdir -p "/home/${FIRST_USER_NAME}/data/user_templates"
+  mkdir -p "/home/${FIRST_USER_NAME}/kenban"
+
+  git clone https://github.com/Kenban/kenban_os.git "/home/${FIRST_USER_NAME}/kenban"
+
+  cp -r "/home/${FIRST_USER_NAME}/kenban/images/*" "/home/${FIRST_USER_NAME}/data/default_images"
+  cp -r "/home/${FIRST_USER_NAME}/kenban/templates/*" "/home/${FIRST_USER_NAME}/data/default_templates"
+  cp "/home/${FIRST_USER_NAME}/kenban/templates/macros.html" "/home/${FIRST_USER_NAME}/data/user_templates/"
+
+  install -m 664 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/.profile" "/home/${FIRST_USER_NAME}/.profile"
+  install -m 664 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/.xinitrc" "/home/${FIRST_USER_NAME}/.xinitrc"
+  install -m 644 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/websocket-sync.service" "/etc/systemd/system/websocket-sync.service"
+  install -m 644 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/websocket-local.service" "/etc/systemd/system/websocket-local.service"
+  install -m 644 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/kenban-wifi-manager.service" "/etc/systemd/system/kenban-wifi-manager.service"
+  install -m 644 "/home/${FIRST_USER_NAME}/kenban/pi-gen-files/kenbanxorg.conf" "/etc/X11/xorg.conf.d/kenban.conf"
+
   raspi-config nonint do_boot_behaviour B2
   sed -i 's/sam/${FIRST_USER_NAME}/g' /etc/systemd/system/getty@tty1.service.d/autologin.conf
   systemctl enable websocket-sync.service local-websocket.service kenban-wifi-manager.service
+
+  mkdir -p /home/${FIRST_USER_NAME}/kenban/local-websocket/nodejs
+  wget https://nodejs.org/dist/v18.13.0/node-v18.13.0-linux-x64.tar.xz
+  tar -xvf node-v18.13.0-linux-x64.tar.xz -C /home/${FIRST_USER_NAME}/kenban/local-websocket/nodejs --strip-components=1
 
   pip install -r /home/${FIRST_USER_NAME}/kenban/requirements.txt
   cd /home/${FIRST_USER_NAME}/kenban/local-websocket && npm install
